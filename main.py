3b7c80aed5989ae98f9fc4e465979f21b3585397
najeebib <najeebibraheem1999@hotmail.com>
2023-11-29 11:16:32 +0200

    added the table of expenses using treeview
    // todo: get the data from db instead of static data

--- main.py
+++ main.py
@@ -1,24 +1,58 @@
 import tkinter as tk
-import ttkbootstrap as ttk
+from tkinter import ttk
+import random
+from datetime import datetime
+
+import ttkbootstrap as ttks
 from ttkbootstrap.constants import *
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+def generate_random_date():
+    start_date = datetime(2022, 1, 1)
+    end_date = datetime(2022, 12, 31)
+    random_date = start_date + (end_date - start_date) * random.random()
+    return random_date.strftime('%Y-%m-%d')
+
+def fill_tree(tree):
+    # Add some random data to the treeview
+    for i in range(1, 200):
+        name = f"Item {i}"
+        date = generate_random_date()
+        tree.insert(parent='', index=tk.END, values=(i, name, date))
 
 window = tk.Tk()
 window.geometry('1600x800')
-
+window_height = window.winfo_screenheight()
+tree_height_percentage = 0.75
+tree_visible_rows = int((window_height * tree_height_percentage) / 20)  # Assuming each row has a height of 20
 
 # Row configuration for the entire window
 window.grid_rowconfigure(0, weight=1)
-window.grid_columnconfigure(0, weight=1)
-window.grid_columnconfigure(1, weight=1)
+window.grid_columnconfigure(0, weight=40)
+window.grid_columnconfigure(1, weight=60)
+
+window_width = window.winfo_screenwidth()
+tree_width_percentage = 0.4
+tree_visible_columns = int((window_width * tree_width_percentage) / 100)  # Assuming each column has a width of 100
+
 # Create frame1 (60% width, full height)
 frame1 = tk.Frame(window)
 frame1.grid(row=0, column=0, sticky="nsew")
 frame1.grid_rowconfigure(0, weight=15)
 frame1.grid_rowconfigure(1, weight=10)
 frame1.grid_rowconfigure(2, weight=75)
-frame1.grid_columnconfigure(0, weight=60)
+frame1.grid_columnconfigure(0, weight=1)
+
+tree = ttk.Treeview(window,columns=('Category', 'Amount', 'Date'), show='headings', height=tree_visible_rows)
+tree.heading("Category", text="Category")
+tree.heading("Amount", text="Amount")
+tree.heading("Date", text="Date")
+tree.column("Category", width=int(tree_visible_columns/3))
+tree.column("Amount", width=int(tree_visible_columns/3))
+tree.column("Date", width=int(tree_visible_columns/3))
+
+tree.grid(row=2, column=0, sticky="nsew")
+fill_tree(tree)
 
 # label_1_0 = tk.Label(frame1, text="Frame 1, Row 0", bg="red")
 # label_1_0.grid(row=0, column=0, sticky="nsew")
@@ -31,10 +65,9 @@ frame1.grid_columnconfigure(0, weight=60)
 # Create frame2 (40% width, full height)
 frame2 = tk.Frame(window)
 frame2.grid(row=0, column=1, sticky="nsew")
-frame2.grid_rowconfigure(0, weight=33)
-frame2.grid_rowconfigure(1, weight=33)
-frame2.grid_rowconfigure(2, weight=33)
-frame2.grid_columnconfigure(0, weight=40)
+frame2.grid_rowconfigure(0, weight=50)
+frame2.grid_rowconfigure(1, weight=50)
+frame2.grid_columnconfigure(0, weight=1)
 
 # label_2_0 = tk.Label(frame2, text="Frame 2, Row 0", bg="yellow")
 # label_2_0.grid(row=0, column=0, sticky="nsew")
@@ -42,8 +75,6 @@ frame2.grid_columnconfigure(0, weight=40)
 # label_2_1 = tk.Label(frame2, text="Frame 2, Row 1", bg="purple")
 # label_2_1.grid(row=1, column=0, sticky="nsew")
 #
-# label_2_2 = tk.Label(frame2, text="Frame 2, Row 2", bg="orange")
-# label_2_2.grid(row=2, column=0, sticky="nsew")
 
 
 window.mainloop()
