5c5159d2e3a8cbf5ddea3a0d48556df7d33967ac
najeebib <najeebibraheem1999@hotmail.com>
2023-12-01 20:28:44 +0200

    display the data from db to the charts

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 import tkinter as tk
 from tkinter import ttk
-import random
-from datetime import datetime
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import mysql.connector
 import os
 from dotenv import load_dotenv
-
-
-def generate_random_date():
-    start_date = datetime(2022, 1, 1)
-    end_date = datetime(2022, 12, 31)
-    random_date = start_date + (end_date - start_date) * random.random()
-    return random_date.strftime('%Y-%m-%d')
 
 
 def fill_tree(tree):
@@ -27,16 +18,26 @@ def fill_tree(tree):
 
 
 def create_pie_chart(frame):
+    for widget in frame.winfo_children():
+        widget.destroy()
+    query1 = "SELECT Category, SUM(Amount) FROM Expenses GROUP BY Category;"
+    data = execute_query(query1)
+    sumOfAmount = []
+    categories = []
+    for tup in data:
+        value1, value2 = tup  # Unpack the tuple into two values
+        categories.append(value1)  # Append the first value to array1
+        sumOfAmount.append(value2)  # Append the second value to array2
+
     fig = Figure(figsize=(frame.winfo_width() / 100, frame.winfo_height() / 100), dpi=100)
     pie_chart = fig.add_subplot(111)
-    sizes = [25, 30, 20, 25]
-    labels = ['A', 'B', 'C', 'D']
-    wedges, texts, autotexts = pie_chart.pie(sizes, labels=labels, autopct='%1.1f%%')
+
+    wedges, texts, autotexts = pie_chart.pie(sumOfAmount, labels=categories, autopct='%1.1f%%')
 
     pie_chart.axis('equal')
 
     # Adding a legend
-    pie_chart.legend(wedges, labels, loc='center left', bbox_to_anchor=(1, 0.5), title='Legend')
+    pie_chart.legend(wedges, categories, loc='center left', bbox_to_anchor=(1, 0.5), title='Legend')
 
     canvas = FigureCanvasTkAgg(fig, master=frame)
     canvas.get_tk_widget().pack(expand=True, fill=tk.BOTH)
@@ -44,11 +45,19 @@ def create_pie_chart(frame):
 
 # Function to create the bar series
 def create_bar_series(frame):
+    for widget in frame.winfo_children():
+        widget.destroy()
+    query2 = "SELECT DATE_FORMAT(TransactionDate, '%m-%Y') AS TransactionMonth, SUM(Amount) AS TotalAmount FROM Expenses GROUP BY DATE_FORMAT(TransactionDate, '%m-%Y') ORDER BY TransactionMonth;"
+    data = execute_query(query2)
+    amount = []
+    categories = []
+    for tup in data:
+        value1, value2 = tup  # Unpack the tuple into two values
+        categories.append(value1)  # Append the first value to array1
+        amount.append(value2)  # Append the second value to array2
     fig = Figure(figsize=(frame.winfo_width() / 100, frame.winfo_height() / 100), dpi=100)
     bar_series = fig.add_subplot(111)
-    categories = ['Category 1', 'Category 2', 'Category 3', 'Category 4']
-    values = [20, 35, 30, 15]
-    bar_series.bar(categories, values)
+    bar_series.bar(categories, amount)
 
     canvas = FigureCanvasTkAgg(fig, master=frame)
     canvas.get_tk_widget().pack(expand=True, fill=tk.BOTH)
@@ -63,6 +72,12 @@ def execute_query(query):
 
     cursor.close()
     return rows
+
+
+def button_clicked():
+    create_pie_chart(sub_frame_1)
+    create_bar_series(sub_frame_2)
+
 
 
 load_dotenv()
@@ -80,19 +95,16 @@ mydb = mysql.connector.connect(
 )
 
 # Queries
-query1 = "SELECT Category, SUM(Amount) FROM Expenses GROUP BY Category;"
-query2 = "SELECT DATE_FORMAT(TransactionDate, '%m-%Y') AS TransactionMonth, SUM(Amount) AS TotalAmount FROM Expenses GROUP BY DATE_FORMAT(TransactionDate, '%m-%Y') ORDER BY TransactionMonth;"
+
 query3 = "SELECT * FROM Expenses;"
 # Execute queries
-execute_query(query1)
-execute_query(query2)
 
 window = tk.Tk()
 window.geometry('1600x800')
 
 window.grid_rowconfigure(0, weight=1)
-window.grid_columnconfigure(0, weight=40)
-window.grid_columnconfigure(1, weight=60)
+window.grid_columnconfigure(0, weight=30)
+window.grid_columnconfigure(1, weight=70)
 
 frame1 = tk.Frame(window)
 frame1.grid(row=0, column=0, sticky="nsew")
@@ -100,6 +112,12 @@ frame1.grid_rowconfigure(0, weight=15)
 frame1.grid_rowconfigure(1, weight=10)
 frame1.grid_rowconfigure(2, weight=75)
 frame1.grid_columnconfigure(0, weight=1)
+
+label = tk.Label(frame1, text="Expense Tracker App", font=("Helveticca", 28), foreground="blue")
+label.grid(row=0, column=0)
+
+button = tk.Button(frame1, text="Add Expense", command=button_clicked, width=10)
+button.grid(row=1, column=0, padx=10, pady=5, columnspan=2)
 
 tree = ttk.Treeview(frame1, columns=('Category', 'Amount', 'Date'), show='headings')
 tree.heading("Category", text="Category")
